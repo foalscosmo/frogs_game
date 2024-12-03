b9793dcb510c0cf4aac9a50b18603b4f742f51e2
foalscosmo <foalscosmo@gmail.com>
2024-12-03 18:26:02 +0400

    initial commit

---
+++ MatchColorFrogsEntryPoint.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using com.appidea.MiniGamePlatform.CommunicationAPI;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class MatchColorFrogsEntryPoint : BaseMiniGameEntryPoint, IMiniGameLoadingProgressHandler
+{
+    private AsyncOperationHandle<GameObject> handle;
+    
+    protected override Task LoadInternal()
+    {
+        handle = Addressables.LoadAssetAsync<GameObject>("Match_Colors_Frogs");
+        return Task.CompletedTask;
+    }
+
+    protected override Task UnloadInternal()
+    {
+        Addressables.Release(handle);
+        return Task.CompletedTask;
+    }
+
+    public float Progress { get; private set; }
+    public event Action<float> ProgressChanged;
+}
